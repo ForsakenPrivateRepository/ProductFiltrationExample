c4007f343fc736a2562d120f14a2faf1d6983886
ReenExe <ipourri@gmail.com>
2015-01-28 02:13:35 +0200

    init

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'src/Filter.php';
+require_once 'src/Generator.php';
+require_once 'vendor/autoload.php';
---
+++ tests/FilterTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class FilterTest extends PHPUnit_Framework_TestCase
+{
+    public function testTime()
+    {
+        $this->assertTrue(true);
+    }
+} 
