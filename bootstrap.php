702c07a2d602d466f0937044a5984ec07d771bb1
ReenExe <ipourri@gmail.com>
2015-01-28 04:00:41 +0200

    for travis

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
 ini_set('memory_limit', '-1');
-require_once __DIR__ . '/src/Filter.php';
-require_once __DIR__ . '/src/Generator.php';
+require_once __DIR__ . '/src/filter.php';
+require_once __DIR__ . '/src/generator.php';
 require_once __DIR__ . '/vendor/autoload.php';
